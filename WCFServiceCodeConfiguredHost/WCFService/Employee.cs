43fdee1cb888a6a0ed0f9758da6921a4a990a135
tavisca-smurkar <smurkar@tavisca.com>
2016-09-08 16:47:13 +0530

    Initial Commit

---
+++ WCFServiceCodeConfiguredHost/WCFService/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+namespace WCFService
+{
+    [DataContract]
+    public class Employee
+    {
+        [DataMember]
+        public string Id { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public DateTime DateOfBirth { get; set; }
+
+        [DataMember]
+        public Gender Gender { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2
+    }
+}
---
+++ WCFServiceCodeConfiguredHost/WCFService/IEmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using System.ServiceModel;
+namespace WCFService
+{
+    [ServiceContract]
+    public interface IEmployeeService
+    {
+
+        [OperationContract]
+        Employee GetEmployee(string id);
+
+        [OperationContract]
+        void SaveEmployee(Employee employee);
+    }
+}
