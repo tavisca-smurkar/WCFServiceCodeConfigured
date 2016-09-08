343dc5b888f60e5071536b3802534174212a2a3c
tavisca-smurkar <smurkar@tavisca.com>
2016-09-08 17:28:29 +0530

    host configured

---
+++ WCFServiceCodeConfiguredHost/EmployeeServiceHost/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+using WCFService;
+
+namespace EmployeeServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(EmployeeService)))
+            {
+                ServiceMetadataBehavior serviceBehaviour = new ServiceMetadataBehavior();
+                serviceBehaviour.HttpsGetEnabled = true;
+                host.Description.Behaviors.Add(serviceBehaviour);
+                host.AddServiceEndpoint(typeof(IEmployeeService), new BasicHttpBinding(), "EmployeeService");
+                host.Open();
+                Console.WriteLine("Service is started at {0}", DateTime.Now);
+                Console.ReadLine();
+            }
+        }
+    }
+}
